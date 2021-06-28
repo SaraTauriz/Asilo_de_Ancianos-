7f75fba07e1897ea911e3f45774a2049fafe8b19
SaraTauriz <56497503+SaraTauriz@users.noreply.github.com>
2021-06-27 21:21:36 -0500

    Add files via upload

---
+++ Consulta 1 .sql
@@ -0,0 +1,19 @@
+--¿Cuál es la cantidad de pacientes que tienen cada uno de los médicos especialistas?
+
+SELECT 
+  count (tipopersonal.tipoper_especialista) as medico_especialista, 
+  personal.per_nombre as nombre_medico_especialista, 
+  count(paciente.pa_nombre) as pacientes
+
+FROM 
+  public.tipopersonal, 
+  public.personal, 
+  public.alimenestablecida, 
+  public.paciente
+WHERE 
+  tipopersonal.tipoper_id = personal.tipoper_id AND
+  personal.per_id = alimenestablecida.per_id AND
+  paciente.pa_id = alimenestablecida.pa_id AND 
+  tipopersonal.tipoper_especialista = 'si'
+
+ Group by(personal.per_nombre)
